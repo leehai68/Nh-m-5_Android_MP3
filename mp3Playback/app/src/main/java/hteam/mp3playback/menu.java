6de73c0eacfdb55a100af2294c1cf3944243d98a
Nguyễn Đoàn Nam <zz_wing_zz_hp95@yahoo.com.vn>
2017-05-04 14:46:55 +0700

    MenuApps
    51314_Nguyen Doan Nam
    -Them giao dien mo dau cho chuong trinh nghe nhac

--- mp3Playback/app/src/main/java/hteam/mp3playback/menu.java
+++ mp3Playback/app/src/main/java/hteam/mp3playback/menu.java
@@ -1,11 +1,20 @@
 package hteam.mp3playback;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hteam.offline.MainListActivity;
 
 public class menu extends AppCompatActivity {
 
@@ -14,6 +23,7 @@ public class menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        checkAndRequestPermissions();
     }
 
     @Override
@@ -33,8 +43,25 @@ public class menu extends AppCompatActivity {
         else if(item.getItemId() == R.id.mnuOffline)
         {
 
+            Intent intent=new Intent(menu.this,MainListActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void checkAndRequestPermissions() {
+        String[] permissions = new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(permission);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 1);
+        }
     }
 }
